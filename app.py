68b31bc4323ab986b0545e228c99420d73623f37
Vemund Santi <vemund@santi.no>
2019-03-20 12:48:02 +0100

    feat: add create react app skeleton

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='frontend/build')
 
+@app.route('/<path:path>')
+def send_js(path):
+    return send_from_directory('frontend/build', path)
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
-
+# Run the app locally
 if __name__ == '__main__':
-    my_awesome_app.run()
+    app.run()
---
+++ frontend/build/precache-manifest.d95f60c641ac4e14f8b7bc58bb7e97e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87186119f50fd3c91a9c",
+    "url": "/static/js/main.7374bade.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "87186119f50fd3c91a9c",
+    "url": "/static/css/main.8dbde0eb.chunk.css"
+  },
+  {
+    "revision": "377345a710ddb8deb972e50d88fcd28c",
+    "url": "/index.html"
+  }
+];
